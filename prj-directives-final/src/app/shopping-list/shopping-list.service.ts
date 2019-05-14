2e169f9ecb27a7e41f84013b06eb4bd1cd0467a2
Dylan Acup <dacup@ciscocems.com>
2019-05-14 14:57:27 -0600

    more work on services

---
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+}
