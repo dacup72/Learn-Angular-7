974b9190b746633ffaad14346c156982a3a22dbd
Dylan Acup <dacup@ciscocems.com>
2019-06-05 13:29:34 -0600

    routing

--- prj-directives-final/src/app/recipes/recipe-list/recipe.service.ts
+++ prj-directives-final/src/app/recipes/recipe-list/recipe.service.ts
@@ -1,17 +1,41 @@
 import { Recipe } from '../recipe.model';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'A Test Recipe', 
+      'This is simply a test', 
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Corn', 4)
+      ]
+    ),
+    new Recipe(
+      'Another Test Recipe', 
+      'This is simply a test', 
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Bun', 2),
+        new Ingredient('Lettuce', 1)
+      ]
+    )
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
